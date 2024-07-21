40e048253127b4859912ef4eeb9fa9ab1db4d605
Francisco Veiras <74626997+francisco-svg761@users.noreply.github.com>
2024-07-21 18:05:44 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use std::{io::{prelude::*, BufReader, Write}, net::TcpListener};
 struct HttpReqLine<'a> {
     method: &'a str,
     target: &'a str,
-    http_version: &'a str
+    http_version: &'a str,
+    user_agent: Option<&'a String>
 }
 
 impl <'a> HttpReqLine<'a> {
-    fn new(method: &'a str, target: &'a str, http_version: &'a str) -> Self {
-        Self { method, target, http_version }
+    fn new(method: &'a str, target: &'a str, http_version: &'a str, user_agent: Option<&'a String>) -> Self {
+        Self { method, target, http_version, user_agent }
     }
 }
 
@@ -25,15 +26,32 @@ fn main() {
                 let response = "HTTP/1.1 200 OK\r\n\r\n";
                 let err_response = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-                let buf_reader = BufReader::new(&mut stream);
-                let request_line = buf_reader.lines().next().unwrap().unwrap();
+                let buf_reader: BufReader<&mut std::net::TcpStream> = BufReader::new(&mut stream);
 
-                let parts = request_line.split(" ").collect::<Vec<_>>();
+                let http_request: Vec<_> = buf_reader
+                    .lines()
+                    .map(|result| result.unwrap())
+                    .take_while(|line| !line.is_empty())
+                    .collect();
 
+                println!("here {:?}", http_request);
+                // let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-                let http_req_line = HttpReqLine::new(parts[0], parts[1], parts[2]);
+                let parts = http_request[0].split(" ").collect::<Vec<_>>();
 
-                if http_req_line.method == "GET" && http_req_line.target == "/" || http_req_line.target.contains("echo") {
+                let user_agent = http_request.iter().find(|x| x.contains("User-Agent"));
+
+                let http_req_line: HttpReqLine = HttpReqLine::new(parts[0], parts[1], parts[2], user_agent);
+
+                if http_req_line.method == "GET" && http_req_line.target == "/" || http_req_line.target.contains("echo") || http_req_line.target.contains("user-agent") {
+                    if http_req_line.user_agent.is_some() && http_req_line.target.contains("user-agent") {
+                        let user_agent_value = http_req_line.user_agent.unwrap().split(": ").collect::<Vec<_>>()[1];
+
+                        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len}\r\n\r\n{body}", len = user_agent_value.len(), body = user_agent_value);
+
+                        stream.write_all(response.as_bytes()).unwrap();
+                    }       
+                    
                     if http_req_line.target.contains("echo") {
                         let path = http_req_line.target.split("/").collect::<Vec<_>>()[2];
 
