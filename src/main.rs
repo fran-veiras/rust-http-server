a94af10cd8f14e05b563b9e533068a770b0c19d1
Francisco Veiras <74626997+francisco-svg761@users.noreply.github.com>
2024-07-21 10:48:10 -0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::Write, net::TcpListener};
+use std::{io::{prelude::*, BufReader, Write}, net::TcpListener};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,8 +10,16 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 let response = "HTTP/1.1 200 OK\r\n\r\n";
+                let err_response = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-                stream.write_all(response.as_bytes()).unwrap();
+                let buf_reader = BufReader::new(&mut stream);
+                let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+                if request_line == "GET / HTTP/1.1" {
+                    stream.write_all(response.as_bytes()).unwrap();
+                } else {
+                    stream.write_all(err_response.as_bytes()).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
